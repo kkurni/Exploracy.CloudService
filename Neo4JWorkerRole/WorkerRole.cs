84903bcdb1363056b004cd0671368c4231881868
kkurni <kkurni@gmail.com>
2012-12-27 12:33:36 +1100

    add neo4j worker server

--- Neo4JWorkerRole/WorkerRole.cs
+++ Neo4JWorkerRole/WorkerRole.cs
@@ -8,11 +8,13 @@ using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.StorageClient;
+using Neo4j.Server.AzureWorkerHost;
 
 namespace Neo4JWorkerRole
 {
     public class WorkerRole : RoleEntryPoint
     {
+        private NeoServer _Server;
         public override void Run()
         {
             // This is a sample worker implementation. Replace with your logic.
@@ -33,7 +35,17 @@ namespace Neo4JWorkerRole
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
+            _Server = new NeoServer(CloudStorageAccount.DevelopmentStorageAccount);
+            _Server.DownloadAndInstall();
+            _Server.Start();
+
             return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            _Server.Stop();
+            base.OnStop();
         }
     }
 }
