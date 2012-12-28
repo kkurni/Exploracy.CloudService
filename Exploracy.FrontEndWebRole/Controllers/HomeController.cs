40f4e0ea6e90ce95079d3479bade5e9451aa0ade
kkurni <kkurni@gmail.com>
2012-12-28 17:23:34 +1100

    add front end web

---
+++ Exploracy.FrontEndWebRole/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exploracy.FrontEndWebRole.Models;
+
+namespace Exploracy.FrontEndWebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("SubmitReview");
+        }
+
+        public ActionResult SubmitReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitReview(ItemReviewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("SubmitReview");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Exploracy.FrontEndWebRole/Models/ItemReviewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exploracy.FrontEndWebRole.Models
+{
+    public class ItemReviewModel
+    {
+        public string ItemName { get; set; }
+        public string Review { get; set; }
+    }
+}
